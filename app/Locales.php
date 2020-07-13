9e84642665e036d9a15e79b95414046fa87fdd7b
asgrim7 <67489176+asgrim7@users.noreply.github.com>
2020-07-13 07:55:22 -0500

    Add files via upload

---
+++ app/Locales.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Locales extends Model
+{
+    //
+    protected $primaryKey = 'LocCod';
+    protected $table = 'locales';
+}
